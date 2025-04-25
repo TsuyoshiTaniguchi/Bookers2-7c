fbafcdce1d668e21eb2ab16cd8cbfc22a6c33e22
TsuyoshiTaniguchi <202054797+TsuyoshiTaniguchi@users.noreply.github.com>
2025-04-25 13:08:32 +0000

    7c Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -15,6 +16,9 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   get "search" => "searches#search"
+
+  resources :groups, except: [:destroy]
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
